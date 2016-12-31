56eb36ae9fc24d65da0b1f380040b78769428d18
Kevin Herrera <kevin@herrera.io>
2016-12-31 10:15:59 -0800

    Creating a callable XML document reader.

---
+++ src/KHerGe/XML/Exception/Reader/InvalidCallableResultException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KHerGe\XML\Exception\Reader;
+
+use KHerGe\XML\Exception\ReaderException;
+
+/**
+ * An exception that is thrown if a callable did not return an XML reader.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class InvalidCallableResultException extends ReaderException
+{
+}
